e3a1237f79ac99fab2d5562d0273ee37b7c72da4
dgpeng <dgpeng@gmail.com>
2017-03-05 01:14:05 -0600

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,10 @@
-#Plot1.R
+#extra data.zip to the working directory; and load data to a short name
+	HPC <- file ("household_power_consumption.txt")
+#read the data from just those two dates (from 2007-02-01 to 2007-02-02)
+	twodates <- read.table (text = grep ("^[1,2]/2/2007", readLines(HPC), value = TRUE), 
+	col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+	sep = ";", header = TRUE)
+#Plotting plot1.png
 	png("plot1.png", width=480, height=480)
 	hist(twodates$Global_active_power, col = "red", main = paste("Global Active Power"), 
 	xlab = "Global Active Power (kilowatts)")
